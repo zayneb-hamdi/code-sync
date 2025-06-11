e648950363a4023f38a1394cca2882fed9326029
zayneb-hamdi <hamdi.zayneb123@gmail.com>
2025-06-11 08:36:10 +0100

    Add solution for best-time-to-buy-and-sell-stock

---
+++ leetcode/best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+      int minprice=Integer.MAX_VALUE;
+      int maxprofit=0;
+      for(int price:prices)
+      {
+        if(price<minprice)
+        {
+            minprice=price;
+        }
+        else if(price-minprice>maxprofit)
+        
+        {
+            maxprofit=price-minprice;
+        }
+      }
+        return maxprofit;
+    }
+}
